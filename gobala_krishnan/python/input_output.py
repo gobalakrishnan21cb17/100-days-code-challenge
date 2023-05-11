30b750d846e3e01babcbffcebe0f6a8d842f40d3
gobalakrishnan21cb17 <kbaskarankbaskaran72@gmail.com>
2023-05-11 19:25:09 +0530

    Input

---
+++ gobala_krishnan/python/input_output.py
@@ -0,0 +1,30 @@
+val = input("Enter your value: ")
+print(val)
+
+# ------------------------------------------------
+num = int(input("Enter a number: "))
+print(num, " ", type(num))
+ 
+# -------------------------------------------------         
+floatNum = float(input("Enter a decimal number: "))
+print(floatNum, " ", type(floatNum))
+
+# -------------------------------------------------
+num1 = int(input())
+num2 = int(input())
+ 
+# printing the sum in integer
+print(num1 + num2)
+
+# -------------------------------------------------
+num1 = float(input())
+num2 = float(input())
+ 
+# printing the sum in float
+print(num1 + num2)
+
+# -------------------------------------------------
+string = str(input())
+print(string)
+string_default = input()
+print(string_default)
