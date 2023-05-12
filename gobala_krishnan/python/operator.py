3e07494d801d5cf7da9a4408b42b1268ab0e39c3
gobalakrishnan21cb17 <kbaskarankbaskaran72@gmail.com>
2023-05-12 06:37:11 +0530

    Day-3

---
+++ gobala_krishnan/python/operator.py
@@ -0,0 +1,115 @@
+# Examples of Arithmetic Operator
+a = 9
+b = 4
+ 
+# Addition of numbers
+add = a + b
+ 
+# Subtraction of numbers
+sub = a - b
+ 
+# Multiplication of number
+mul = a * b
+ 
+# Modulo of both number
+mod = a % b
+ 
+# Power
+p = a ** b
+ 
+# print results
+print(add)
+print(sub)
+print(mul)
+print(mod)
+print(p)
+
+# ---------------------------------------------------
+
+
+# Examples of Relational Operators
+a = 13
+b = 33
+ 
+# a > b is False
+print(a > b)
+ 
+# a < b is True
+print(a < b)
+ 
+# a == b is False
+print(a == b)
+ 
+# a != b is True
+print(a != b)
+ 
+# a >= b is False
+print(a >= b)
+ 
+# a <= b is True
+print(a <= b)
+
+# -------------------------------------------------------
+
+a = True
+b = False
+ 
+# Print a and b is False
+print(a and b)
+ 
+# Print a or b is True
+print(a or b)
+ 
+# Print not a is False
+print(not a)
+
+# ---------------------------------------------------------------
+
+
+# Examples of Bitwise operators
+a = 10
+b = 4
+ 
+# Print bitwise AND operation
+print(a & b)
+ 
+# Print bitwise OR operation
+print(a | b)
+ 
+# Print bitwise NOT operation
+print(~a)
+ 
+# print bitwise XOR operation
+print(a ^ b)
+ 
+# print bitwise right shift operation
+print(a >> 2)
+ 
+# print bitwise left shift operation
+print(a << 2)
+
+# -------------------------------------------------------------------
+
+
+# Examples of Assignment Operators
+a = 10
+ 
+# Assign value
+b = a
+print(b)
+ 
+# Add and assign value
+b += a
+print(b)
+ 
+# Subtract and assign value
+b -= a
+print(b)
+ 
+# multiply and assign
+b *= a
+print(b)
+ 
+# bitwise lishift operator
+b <<= a
+print(b)
