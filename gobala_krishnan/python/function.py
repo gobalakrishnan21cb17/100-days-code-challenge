3f160ce2b45ce23d7e57ae1f01568fc4114ede22
gobalakrishnan21cb17 <kbaskarankbaskaran72@gmail.com>
2023-05-18 23:52:21 +0530

    first class function

--- gobala_krishnan/python/function.py
+++ gobala_krishnan/python/function.py
@@ -41,3 +41,20 @@ def f():
 s = "I love Geeksforgeeks"
 f()
 print("Outside Function", s)
+
+# ------------------------------------------
+
+def shout(text):
+    return text.upper()
+  
+def whisper(text):
+    return text.lower()
+  
+def greet(func):
+    # storing the function in a variable
+    greeting = func("""Hi, I am created by a function
+                    passed as an argument.""")
+    print (greeting) 
+  
+greet(shout)
+greet(whisper)
